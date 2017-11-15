2045167c9c8ff83ffe5ce1a9621ee523671b1fc3
EraGoN2311 <33679291+EraGoN2311@users.noreply.github.com>
2017-11-15 08:54:40 +0330

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,6 +1,8 @@
 /*The MIT License (MIT)
 
 Copyright (c) 2015 Apostolique
+EraGoN2311/Agar.io
+
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
